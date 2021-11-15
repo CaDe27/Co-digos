10aa08a572b8ae0e7aabc74629b8ffceb4c0c32b
CaDe27 <cdelgadoelizondo@gmail.com>
2021-11-15 01:12:59 -0600

    nuevos problemas

--- ICPC/PrimeraFecha2021/pf21/prueba.cpp
+++ ICPC/PrimeraFecha2021/pf21/prueba.cpp
@@ -31,74 +31,11 @@ typedef vector<int, pii> viii;
 
 #define loop(i,a,b) for(ll i = a; i < b; ++i)
 
-const int maxP = 1e6+10;
 int n, m;
-
-
-
 void solve(){
    
 }
 
-int aInt(string x){
-    int resp = 0;
-    for(char c:x)
-        resp = resp*10 + (c-'0');
-    return resp;
-}
-
-string sumTiempo(string x, int s, int mili){
-    int milX=aInt(x.substr(9,3));
-    int mil = mili;
-
-    int seg= s;
-    int segX=aInt(x.substr(6,2));
-
-    int minuteX=aInt(x.substr(3,2));
-    int hourX=aInt(x.substr(0,2));
-
-    if(mil+milX>999){
-        segX+=1;
-    }
-    milX=(mil+milX)%1000;
-    if(segX+seg>59){
-        minuteX+=1;
-    }
-    segX=(segX+seg)%60;
-
-    if(minuteX>59){
-        hourX+=1;
-    }
-    minuteX %= 60;
-
-    string hX = to_string(hourX), mX = to_string(minuteX), sX = to_string(segX), miliX = to_string(milX);
-    while(hX.size() < 2)
-        hX = "0" + hX;
-
-    while(mX.size() < 2)
-        mX = "0" + mX;  
-
-    while(sX.size() < 2)
-        sX = "0" + sX;  
-
-    while(miliX.size() < 3)
-        miliX = "0" + miliX;    
-
-    return hX+":"+mX+":"+sX+","+miliX;
-    //00:00:00,000
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    ll b = 1e9;
-    b = b*(50000)*(100000+1);
-    cout<<b<<endl;
-    return 0;
-
-}
-/*
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     //if(fopen("case.txt", "r")) freopen("case.txt", "r", stdin);
@@ -107,4 +44,4 @@ int main(){
         solve();
     }
     return 0; 
-}*/
+}
